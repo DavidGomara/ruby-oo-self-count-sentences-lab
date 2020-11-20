da95a65f0f8c2bf95ca1d702340eae90082324dd
David Gomara <davidgomara7@gmail.com>
2020-11-20 03:50:34 +0000

    commit tree 34d67ac828e0657d09a37f7d7abf1213c2a605f6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.split(//).last == "."
   end
 
   def question?
-
+    self.split(//).last == "?"
   end
 
   def exclamation?
-
+    self.split(//).last == "!"
   end
 
   def count_sentences
-
+    count = 0
+    counter = 0
+    array = [".", "!", "?"]
+    self.split(//).each do |x|
+      if x == self.split(//)[counter - 1]
+      elsif array.include?(x)
+        count += 1
+      end 
+      counter += 1
+    end
+    count
   end
 end
